33fa6b6ac45a27ccab322e2999a6f6f9e75d1ad3
518H0090 <nhoxhieuro5@gmail.com>
2022-09-23 17:01:12 +0700

    Banner Menu Tag

--- ShoppeeFood_FE/Template_v3.0/js/DomProcess.js
+++ ShoppeeFood_FE/Template_v3.0/js/DomProcess.js
@@ -55,25 +55,3 @@ const navbarLogo = document.querySelector(".navbar-logo");
 navbarLogo.addEventListener("click", (e) => {
   window.location.href = "./index.html";
 });
-
-// Show Modals Banner Menu Tag
-const bannerMenuTab = document.querySelector(".banner-menu__tab");
-const modalBannerTag = document.querySelector(".modal-bannertag");
-const modalBannerTagOverlay = document.querySelector(
-  ".modal-bannertag__overlay"
-);
-const modalBannerCloseIcon = document.querySelector(
-  ".modal-banner__close-icon"
-);
-
-bannerMenuTab.addEventListener("click", (e) => {
-  modalBannerTag.classList.add("show-tag");
-});
-
-modalBannerTagOverlay.addEventListener("click", (e) => {
-  modalBannerTag.classList.remove("show-tag");
-});
-
-modalBannerCloseIcon.addEventListener("click", (e) => {
-  modalBannerTag.classList.remove("show-tag");
-});
