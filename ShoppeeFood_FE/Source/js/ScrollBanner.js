d634d7586f2f6b4c975eea376ac301dca9e0582e
Hieu Huynh <hieu.huynh@s3corp.com.vn>
2022-09-30 13:16:09 +0700

    ok

--- ShoppeeFood_FE/Source/js/ScrollBanner.js
+++ ShoppeeFood_FE/Source/js/ScrollBanner.js
@@ -3,21 +3,18 @@ const layoutBanner = document.querySelector('.layout-banner');
 const layoutBannerSearch = document.querySelector('.layout-banner__search');
 
 window.addEventListener('scroll',(e) => {
-  if (window.pageYOffset > 30) {
+  if (window.pageYOffset > 10) {
     layoutBannerSearch.classList.add('layout-banner__search--stuck')
-    layoutBannerSearch.classList.remove('layout-banner__search--fixed')
     layoutBanner.style.overflowY = "hidden"
-  } else if (layoutBanner.scrollHeight > 200 ){
-    layoutBannerSearch.classList.add('layout-banner__search--fixed')
+  } else {
     layoutBannerSearch.classList.remove('layout-banner__search--stuck')
     layoutBanner.style.overflowY = "scroll"
-  }
+  } 
 })
 
 window.addEventListener('load',(e) => {
-  
 
-  if (window.pageYOffset >50) {
+  if (window.pageYOffset > 10) {
     layoutBannerSearch.classList.add('layout-banner__search--stuck')
     layoutBanner.style.overflowY = "hidden"
     window.scroll(0,0)
