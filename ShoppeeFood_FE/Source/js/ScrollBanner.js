4c99c9122c5004153446bc891544f3943d872ef1
Hieu Huynh <hieu.huynh@s3corp.com.vn>
2022-09-30 11:00:40 +0700

    add collection tab and fix position for banner search

--- ShoppeeFood_FE/Source/js/ScrollBanner.js
+++ ShoppeeFood_FE/Source/js/ScrollBanner.js
@@ -3,11 +3,11 @@ const layoutBanner = document.querySelector('.layout-banner');
 const layoutBannerSearch = document.querySelector('.layout-banner__search');
 
 window.addEventListener('scroll',(e) => {
-  if (window.pageYOffset > 40) {
+  if (window.pageYOffset > 30) {
     layoutBannerSearch.classList.add('layout-banner__search--stuck')
     layoutBannerSearch.classList.remove('layout-banner__search--fixed')
     layoutBanner.style.overflowY = "hidden"
-  } else if (layoutBanner.scrollHeight > 300 ){
+  } else if (layoutBanner.scrollHeight > 200 ){
     layoutBannerSearch.classList.add('layout-banner__search--fixed')
     layoutBannerSearch.classList.remove('layout-banner__search--stuck')
     layoutBanner.style.overflowY = "scroll"
@@ -17,10 +17,9 @@ window.addEventListener('scroll',(e) => {
 window.addEventListener('load',(e) => {
   
 
-  if (window.pageYOffset >100) {
+  if (window.pageYOffset >50) {
     layoutBannerSearch.classList.add('layout-banner__search--stuck')
     layoutBanner.style.overflowY = "hidden"
     window.scroll(0,0)
   }
 })
-
