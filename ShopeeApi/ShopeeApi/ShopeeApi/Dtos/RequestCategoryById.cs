6a82c1536dbfe6092152c449ffe58ee8888cbf09
Hieu Huynh <hieu.huynh@s3corp.com.vn>
2022-10-24 14:21:22 +0700

    Latest Version

--- ShopeeApi/ShopeeApi/ShopeeApi/Dtos/RequestCategoryById.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShopeeApi.Dtos
-{
-    public class RequestCategoryById
-    {
-        public int CategoryId { get; set; }
-
-        public int RestaurantId { get; set; }
-    }
-}
--- ShopeeApi/ShopeeApi/ShopeeApi/Dtos/RequestCategoryByTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShopeeApi.Dtos
-{
-    public class RequestCategoryByTag
-    {
-        public string CategoryTag { set; get; } = string.Empty;
-
-        public int RestaurantId { get; set; }
-    }
-}
